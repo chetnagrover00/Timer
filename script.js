a9ff4529e91b5a7c450a6b2dc62de1629c9d16fe
chetnagrover00 <65505815+chetnagrover00@users.noreply.github.com>
2020-12-25 11:05:54 +0530

    Timer

---
+++ script.js
@@ -0,0 +1,21 @@
+let lastd= new Date("jan 1, 2040 00:00:00").getTime();
+
+let x =setInterval(function(){
+
+let now=new Date().getTime();
+let diff= lastd - now;
+
+let days =Math.floor (diff/(1000*60*60*24));
+console.log(days);
+let hours = Math.floor ((diff % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+console.log(hours);
+let minutes = Math.floor ((diff% (1000*60*60))/(1000*60));
+console.log (minutes);
+let seconds =Math.floor ((diff%(1000*60))/(1000));
+console.log(seconds);
+
+
+document.getElementById("demo").innerHTML=days+"days,"+hours+" hrs: "+minutes+"m:"+seconds+"s"
+
+
+}, 1000);
